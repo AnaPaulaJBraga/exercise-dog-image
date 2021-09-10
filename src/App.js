30b9a8e248a52b53df9807a25e00402e197b83a1
AnaPaulaBraga <anapaula.junq@hotmail.com>
2021-09-10 19:16:05 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: '',
+    };
+
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.data.message.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    const { data } = this.state;
+    localStorage.setItem('dogURL', data.message);
+    const raçaDog = data.message.split('/')[4];
+    alert(`A raça do doguinho é ${raçaDog}`);
+  }
+
+  async fetchDog() {
+    const requestReturn = await fetch('https://dog.ceo/api/breeds/image/random');
+    const requestObject = await requestReturn.json();
+    this.setState({
+      data: requestObject,
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+    if (data === '') return 'loading...';
+    return (
+      <div>
+        <p> Doguinhos</p>
+
+        <button
+          type="submit"
+          onClick={ this.fetchDog }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Novo doguinho
+        </button>
+        <div>
+          <img src={ data.message } alt="random dog" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
